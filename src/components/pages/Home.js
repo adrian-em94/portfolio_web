a9671bd174bdabef08e021c77c52c25691982313
Adrian <adrianmartin2394@gmail.com>
2021-07-24 14:35:06 -0700

    added techsection component to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import '../../App.css'
 import HomeSection from '../HomeSection'
 import Cards from '../Cards'
+import TechSection from '../TechSection'
 
 function Home() {
     return (
         <>
             <HomeSection />
+            <TechSection />
             <Cards />
+            
         </>
     )
 }
