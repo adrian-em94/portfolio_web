aa3746849f8e956a8464285c6b032702e497572c
Adrian <adrianmartin2394@gmail.com>
2021-07-24 14:34:39 -0700

    created techsection for homepage listing languages

---
+++ src/components/TechSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../components/TechSection.css'
+
+
+function TechSection() {
+    return (
+        <div className="tech_container">
+            <h2 className='tech_heading'>Technologies</h2>
+            <div className='main_container'>
+                <div className="mini_container">
+                                <h3 className='smaller_tech_heading'>Front-End</h3>
+                                <ul className='list_indent'>
+                                    <li>React</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                </ul>
+                            </div>
+                            <div className="mini_container">
+                                <h3 className='smaller_tech_heading'>Back-End</h3>
+                                <ul className='list_indent'>
+                                    <li>JavaScript</li>
+                                    <li>Python</li>
+                                    <li>C++</li>
+                                    <li>Java</li>
+                                    <li>SQL</li>
+                                </ul>
+                            </div>
+            </div>    
+        </div>
+    )
+}
+
+export default TechSection;
