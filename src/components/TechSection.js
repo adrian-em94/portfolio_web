7c73105035d0ebd7d291e0ad0036fa8835e6d4d2
Adrian <adrianmartin2394@gmail.com>
2021-07-25 12:06:14 -0700

    adjusted white space

--- src/components/TechSection.js
+++ src/components/TechSection.js
@@ -8,23 +8,23 @@ function TechSection() {
             <h2 className='tech_heading'>Technologies</h2>
             <div className='main_container'>
                 <div className="mini_container">
-                                <h3 className='smaller_tech_heading'>Front-End</h3>
-                                <ul className='list_indent'>
-                                    <li>React</li>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                </ul>
-                            </div>
-                            <div className="mini_container">
-                                <h3 className='smaller_tech_heading'>Back-End</h3>
-                                <ul className='list_indent'>
-                                    <li>JavaScript</li>
-                                    <li>Python</li>
-                                    <li>C++</li>
-                                    <li>Java</li>
-                                    <li>SQL</li>
-                                </ul>
-                            </div>
+                    <h3 className='smaller_tech_heading'>Front-End</h3>
+                        <ul className='list_indent'>
+                            <li>React</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ul>
+                </div>
+                <div className="mini_container">
+                    <h3 className='smaller_tech_heading'>Back-End</h3>
+                    <ul className='list_indent'>
+                        <li>JavaScript</li>
+                        <li>Python</li>
+                        <li>C++</li>
+                        <li>Java</li>
+                        <li>SQL</li>
+                    </ul>
+                </div>
             </div>    
         </div>
     )
