7f621bb17841ada274fb9f5cefbd94e29ffe8119
Adrian <adrianmartin2394@gmail.com>
2021-07-28 15:12:40 -0700

    none

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Link, } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './Cards.css'
-
-
-
 function CardItem(props) {
     return (   
         <>
