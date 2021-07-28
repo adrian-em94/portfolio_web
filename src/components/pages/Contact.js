ea3cec3e427193c1490782cd7bbf1305e8f8d186
Adrian <adrianmartin2394@gmail.com>
2021-07-28 15:08:40 -0700

    created contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactSection from '../Contact/ContactSection'
+
+function Contact() {
+    return (
+        <>
+            <ContactSection />
+        </>
+    )
+}
+
+export default Contact
