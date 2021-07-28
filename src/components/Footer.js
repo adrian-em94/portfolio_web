8b948dbbf8a8dbfdc1fea9fda1f30ac3a022fb18
Adrian <adrianmartin2394@gmail.com>
2021-07-28 15:10:44 -0700

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Footer.css'
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+
+
+function Footer() {
+    return (
+        <div className='footer_container'>
+            <div className='content'>
+                <h3>Adrian</h3>
+                <h4>Tech Support & Front End Developer</h4>
+                <div className="links">
+                    <Link className='a_link' to="/Projects">
+                        Projects
+                    </Link>
+                    <Link className='a_link' to="/Skills">
+                        Skills
+                    </Link>
+                    <Link className='a_link'to="/">
+                        Contact Me
+                    </Link> 
+                </div>
+                <div className='socials'>
+                    <Link className='soc_link' to={{ pathname: 'https://www.linkedin.com/in/adrian-martin-0906a4bb/' }} target="_blank" rel="noreferrer">
+                        <FaLinkedin  size="2em"/>
+                    </Link>
+                    <Link className='soc_link' to={{ pathname: 'https://github.com/adrian-em94' }} target="_blank" rel="noreferrer">
+                        <FaGithub  size="2em"/>
+                    </Link>
+                </div>   
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default Footer;
