8c0bc6fd3d01fadeac67837a0c71b294bd12e539
Adrian <adrianmartin2394@gmail.com>
2021-07-30 22:59:54 -0700

    added functionality to inputs and state

--- src/components/Contact/InputForm.js
+++ src/components/Contact/InputForm.js
@@ -14,32 +14,36 @@ const InputForm = () => {
     useEffect(() => {}, [newUser]);
 
     const handleOnChange = (event) => {
-        console.log(event.target)
-        const { id, value } = event.target;
+        const { name, value } = event.target;
 
-        //setNewUser({...newUser, [id]: value});
-        console.log(newUser)
-        
-        
+        setNewUser({...newUser, [name]: value}); 
+    }
+
+    const handleSubmit = (event) => {
+        console.log(newUser.name, newUser.email, newUser.proposal)
     }
 
     return (
         <>
             <div className='bootstrap-iso'>
-               <div className="form-floating mb-3">
-                    <input className="form-control" id="floatingInput" placeholder="Name" value={newUser.name} onChange={handleOnChange}/>
-                    <label for="floatingInput">Name</label>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-floating mb-3">
+                        <input className="form-control" name="name"  value={newUser.name} onChange={handleOnChange} placeholder="Name"/>
+                        <label for="floatingInput">Name</label>
+                    </div>
 
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingEmail" placeholder="Email" onChange={handleOnChange}/>
-                    <label for="floatingPassword">Email</label>
-                </div> 
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" name="email" value={newUser.email} onChange={handleOnChange} placeholder="Email" />
+                        <label for="floatingPassword">Email</label>
+                    </div> 
 
-                <div class="form-floating mb-3">
-                    <textarea class="form-control" placeholder="How can I help you?" id="floatingTextarea2" onChange={handleOnChange} style={{height: "100px"}}/>
-                    <label for="floatingTextarea2">How can I help you?</label>
-                </div>
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" name="proposal" value={newUser.proposal} onChange={handleOnChange} placeholder="How can I help you?"  style={{height: "100px"}}/>
+                        <label for="floatingTextarea2">How can I help you?</label>
+                    </div>  
+
+                    <button className='submit-btn' type='button' value='Submit' onClick={handleSubmit}>Submit</button> 
+               </form>
             </div>
             
         </>
