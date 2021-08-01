ba08f43786165a5285569339f03c6d6d5a963747
Adrian <adrianmartin2394@gmail.com>
2021-07-31 21:06:17 -0700

    changed indentation

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -5,10 +5,10 @@ function ImageItem(props) {
     return (   
         <>
             <figure className="image__item__pic-wrap">
-                        <img src={props.src} alt={props.alt} className='image__item__img' />
-                        <div className='image__item__info'>
-                            <p className="image__item__text">{props.text}</p>
-                        </div>
+                <img src={props.src} alt={props.alt} className='image__item__img' />
+                <div className='image__item__info'>
+                    <p className="image__item__text">{props.text}</p>
+                </div>
             </figure>
         </>
     )
