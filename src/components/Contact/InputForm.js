13189f5806caefb59bef953410186e62920c4e85
Adrian <adrianmartin2394@gmail.com>
2021-07-31 21:08:15 -0700

    emailjs send form email

--- src/components/Contact/InputForm.js
+++ src/components/Contact/InputForm.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState, useEffect } from 'react'
 import './InputForm.css'
+import emailjs from 'emailjs-com'
 
 const initialState = {
     name: "",
@@ -20,26 +21,35 @@ const InputForm = () => {
     }
 
     const handleSubmit = (event) => {
-        console.log(newUser.name, newUser.email, newUser.proposal)
+        event.preventDefault()
+
+        emailjs.sendForm(
+            `${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, 
+            `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`, 
+            '#myform', 
+            `${process.env.REACT_APP_EMAILJS_USER_ID}`
+        ).then(res=>{
+            console.log(res)
+        }).catch(err=> console.log(err))
     }
 
     return (
         <>
             <div className='bootstrap-iso'>
-                <form onSubmit={handleSubmit}>
+                <form id='myform' onSubmit={handleSubmit}>
                     <div className="form-floating mb-3">
                         <input className="form-control" name="name"  value={newUser.name} onChange={handleOnChange} placeholder="Name"/>
-                        <label for="floatingInput">Name</label>
+                        <label htmlFor="floatingInput">Name</label>
                     </div>
 
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" name="email" value={newUser.email} onChange={handleOnChange} placeholder="Email" />
-                        <label for="floatingPassword">Email</label>
+                    <div className="form-floating mb-3">
+                        <input type="email" className="form-control" name="email" value={newUser.email} onChange={handleOnChange} placeholder="Email" />
+                        <label htmlFor="floatingPassword">Email</label>
                     </div> 
 
-                    <div class="form-floating mb-3">
-                        <textarea class="form-control" name="proposal" value={newUser.proposal} onChange={handleOnChange} placeholder="How can I help you?"  style={{height: "100px"}}/>
-                        <label for="floatingTextarea2">How can I help you?</label>
+                    <div className="form-floating mb-3">
+                        <textarea className="form-control" name="proposal" value={newUser.proposal} onChange={handleOnChange} placeholder="How can I help you?"  style={{height: "100px"}}/>
+                        <label htmlFor="floatingTextarea2">How can I help you?</label>
                     </div>  
 
                     <button className='submit-btn' type='button' value='Submit' onClick={handleSubmit}>Submit</button> 
