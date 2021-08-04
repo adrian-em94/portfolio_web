f566878ad4a69acd964ece6144c6a1385b0c9f5f
Adrian <adrianmartin2394@gmail.com>
2021-08-04 13:15:23 -0700

    added attributions fixed contact link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,18 +18,24 @@ function Footer() {
                     <Link className='a_link' to="/Skills">
                         Skills
                     </Link>
-                    <Link className='a_link'to="/">
+                    <Link className='a_link'to="/Contact">
                         Contact Me
                     </Link> 
                 </div>
-                <div className='socials'>
-                    <Link className='soc_link' to={{ pathname: 'https://www.linkedin.com/in/adrian-martin-0906a4bb/' }} target="_blank" rel="noreferrer">
-                        <FaLinkedin  size="2em"/>
-                    </Link>
-                    <Link className='soc_link' to={{ pathname: 'https://github.com/adrian-em94' }} target="_blank" rel="noreferrer">
-                        <FaGithub  size="2em"/>
-                    </Link>
-                </div>   
+                <div className='smaller_icons'>
+                    <div className='socials'>
+                        <Link className='soc_link' to={{ pathname: 'https://www.linkedin.com/in/adrian-martin-0906a4bb/' }} target="_blank" rel="noreferrer">
+                            <FaLinkedin  size="2em"/>
+                        </Link>
+                        <Link className='soc_link' to={{ pathname: 'https://github.com/adrian-em94' }} target="_blank" rel="noreferrer">
+                            <FaGithub  size="2em"/>
+                        </Link>
+                    </div>
+                    <div className="attributions">
+                        <p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+                    </div>
+                </div>
+                   
             </div>
             
             
