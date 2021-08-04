1bb34523b09194ca0a0ec4ecd95c9a03b662cfc9
Adrian <adrianmartin2394@gmail.com>
2021-08-04 13:13:57 -0700

    added validation to form and error messages to name and message empty, emal already has bootstrap error for @

--- src/components/Contact/InputForm.js
+++ src/components/Contact/InputForm.js
@@ -7,6 +7,8 @@ const initialState = {
     name: "",
     email: "",
     proposal: "",
+    nameError: "",
+    proposalError: ""
 }
 
 const InputForm = () => {
@@ -20,17 +22,51 @@ const InputForm = () => {
         setNewUser({...newUser, [name]: value}); 
     }
 
+    const validate = () => {
+        let nameError = "";
+        let proposalError = "";
+
+        if(!newUser.name){
+            nameError = 'Name is empty'
+        }
+
+        if(!newUser.proposal){
+            proposalError = "No message being sent"
+        }
+
+
+        if(nameError || proposalError) {
+            setNewUser({...newUser, nameError, proposalError})
+            return false;
+        }
+        return true;
+
+
+    }
+
     const handleSubmit = (event) => {
+        //prevents default html submit form action of clearing form
         event.preventDefault()
 
-        emailjs.sendForm(
+        //checks to see if name, email, and message is not empty
+        const isValid = validate();
+
+        if(isValid){
+            //send to emailjs
+            emailjs.sendForm(
             `${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, 
             `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`, 
             '#myform', 
             `${process.env.REACT_APP_EMAILJS_USER_ID}`
-        ).then(res=>{
-            console.log(res)
-        }).catch(err=> console.log(err))
+            ).then(res=>{
+                console.log(res)
+            }).catch(err=> console.log(err))
+            
+            //reset state
+            setNewUser(initialState)
+        }
+
+        
     }
 
     return (
@@ -41,18 +77,22 @@ const InputForm = () => {
                         <input className="form-control" name="name"  value={newUser.name} onChange={handleOnChange} placeholder="Name"/>
                         <label htmlFor="floatingInput">Name</label>
                     </div>
-
+                    <div className='ErrorMessages'>
+                        {newUser.nameError}
+                    </div>
                     <div className="form-floating mb-3">
                         <input type="email" className="form-control" name="email" value={newUser.email} onChange={handleOnChange} placeholder="Email" />
                         <label htmlFor="floatingPassword">Email</label>
+                    </div>
+                    <div className='ErrorMessages'>
+                        {newUser.proposalError}
                     </div> 
-
                     <div className="form-floating mb-3">
                         <textarea className="form-control" name="proposal" value={newUser.proposal} onChange={handleOnChange} placeholder="How can I help you?"  style={{height: "100px"}}/>
                         <label htmlFor="floatingTextarea2">How can I help you?</label>
                     </div>  
 
-                    <button className='submit-btn' type='button' value='Submit' onClick={handleSubmit}>Submit</button> 
+                    <button className='submit-btn' type='submit' value='Submit'>Submit</button> 
                </form>
             </div>
             
