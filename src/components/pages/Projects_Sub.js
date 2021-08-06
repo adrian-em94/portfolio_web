ab31a8251f9ed084149dfcb901d44f9df86476d8
Adrian <adrianmartin2394@gmail.com>
2021-08-06 16:08:59 -0700

    es6

--- src/components/pages/Projects_Sub.js
+++ src/components/pages/Projects_Sub.js
@@ -3,12 +3,14 @@ import '../../App.css'
 import ARProjSection from '../Projects_Sub/ARProjSection'
 import PortfolioSection from '../Projects_Sub/PortfolioSection'
 
-function Projects_Sub() {
+const Projects_Sub = () => {
     return (
-        <>
+         <>
             <ARProjSection />
             <PortfolioSection />
-        </>
+        </>  
+            
+            
     )
 }
 
