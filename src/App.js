e28a950fe268cbee559398d73c30255d9aa8eb5b
Adrian <adrianmartin2394@gmail.com>
2021-08-07 00:11:24 -0700

    added dependencies

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
-import Projects_Sub from './components/pages/Projects_Sub';
-import ARProjSection from './components/Projects_Sub/ARProjSection';
+import ARProjSection from './components/Projects/ARProjSection';
 import Skills from './components/pages/Skills';
 import VirtualBox from './components/pages/VirtualBox';
 import Footer from './components/Footer';
@@ -22,7 +21,6 @@ function App() {
         <Switch>
           <Route path="/" exact component= {Home} />
           <Route path="/Projects" component= {Projects} />
-          <Route path="/Projects_Sub" component= {Projects_Sub} />
           <Route path="/Portfolio_Project" component= {ARProjSection} />
           <Route path="/Skills" component= {Skills} />
           <Route path="/VirtualBox" component= {VirtualBox} />
