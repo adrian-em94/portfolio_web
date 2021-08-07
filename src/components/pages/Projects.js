a5fff1f12eefc5a4475322a6094cc835705a5a4c
Adrian <adrianmartin2394@gmail.com>
2021-08-07 00:10:26 -0700

    added tabs and moved all content to one page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import '../../App.css'
-import ProjectSection from '../ProjectSection'
+import ARProjSection from '../Projects/ARProjSection'
+import PortfolioSection from '../Projects/PortfolioSection'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import '../Projects/Projects.css'
 
-
-function Projects() {
+const Projects = () => {
     return (
-        <>
-            <ProjectSection />
-        </>
+        <div className='outer_container'>
+            <div className='inner_container'>
+               <Tabs selectedTabClassName="bg-black">
+                <TabList className='testing'>
+                <Tab className="test">ARTIC App</Tab>
+                <Tab className="test">Portfolio Website</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <ARProjSection />
+                </TabPanel>
+                <TabPanel>
+                    <PortfolioSection />
+                </TabPanel>
+                </Tabs>  
+             </div>
+             
+        </div>  
     )
 }
 
