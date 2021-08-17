1b711e07d33cec1b1eb067444cda238cc7b8856a
adrian-em94 <87573904+adrian-em94@users.noreply.github.com>
2021-08-17 00:46:34 -0700

    new routes for new pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/pages/Home';
-import Projects from './components/pages/Projects';
-import ARProjSection from './components/Projects/ARProjSection';
-import Skills from './components/pages/Skills';
-import VirtualBox from './components/pages/VirtualBox';
-import Footer from './components/Footer';
-import Contact from './components/pages/Contact';
-import ActiveDirectory from './components/pages/ActiveDirectory'
-import ADAccountCreation from './components/pages/ADAccountCreation';
-import Lab from './components/pages/Lab';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import ARProjSection from "./components/Projects/ARProjSection";
+import Skills from "./components/pages/Skills";
+import VirtualBox from "./components/pages/VirtualBox";
+import Footer from "./components/Footer";
+import Contact from "./components/pages/Contact";
+import ActiveDirectory from "./components/pages/Skills/ActiveDirectory";
+import ADAccountCreation from "./components/pages/Skills/ADAccountCreation";
+import Lab from "./components/pages/Skills/Lab";
+import SharedFolder from "./components/pages/Skills/SharedFolder";
 
 function App() {
   return (
@@ -19,19 +20,19 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component= {Home} />
-          <Route path="/Projects" component= {Projects} />
-          <Route path="/Portfolio_Project" component= {ARProjSection} />
-          <Route path="/Skills" component= {Skills} />
-          <Route path="/VirtualBox" component= {VirtualBox} />
-          <Route path='/ActiveDirectory' component={ActiveDirectory} />
-          <Route path="/Contact" component= {Contact} />
+          <Route path="/" exact component={Home} />
+          <Route path="/Projects" component={Projects} />
+          <Route path="/Portfolio_Project" component={ARProjSection} />
+          <Route path="/Skills" component={Skills} />
+          <Route path="/VirtualBox" component={VirtualBox} />
+          <Route path="/ActiveDirectory" component={ActiveDirectory} />
+          <Route path="/Contact" component={Contact} />
           <Route path="/ADAccountCreation" component={ADAccountCreation} />
           <Route path="/Lab" component={Lab} />
+          <Route path="/SharedFolder" component={SharedFolder} />
         </Switch>
         <Footer />
       </Router>
-      
     </>
   );
 }
