ae71b336c047f84a013bfd60532d38fe044fb4c3
adrian-em94 <87573904+adrian-em94@users.noreply.github.com>
2021-08-17 00:46:25 -0700

    es6, added more cards to skill section for new pages

--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -1,50 +1,56 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css'
-import VBox from '../images/virtualbox-icon.svg'
-import ADPic from '../images/microsoftad-logo.svg'
-import AddUser from '../images/AddUser.png'
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+import VBox from "../images/virtualbox-icon.svg";
+import ADPic from "../images/microsoftad-logo.svg";
+import AddUser from "../images/AddUser.png";
 
-function SkillSection() {
-
-    return (
-        <div className="cards">
-            <h1>Skills</h1>
-            <div className='cards__container'>
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        <CardItem
-                            src={VBox}
-                            text="Virtual Box - Virtual Machine"
-                            label="Virtualization"
-                            path='/VirtualBox'
-                        />
-                        <CardItem
-                            src={ADPic}
-                            text="Active Directory Domain Services"
-                            label="AD"
-                            path='/ActiveDirectory'
-                        />
-                        <CardItem
-                            src={AddUser}
-                            text="Active Directory Account Creation"
-                            label="Add User"
-                            path='/ADAccountCreation'
-                        />
-                    </ul>
-                </div>
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        <CardItem
-                            src={VBox}
-                            text="Lab Environment w/ VirtualBox Windows Server on MAC"
-                            label="Lab"
-                            path='/Lab'/>
-                    </ul>
-                </div>
-            </div>
+const SkillSection = () => {
+  return (
+    <div className="cards">
+      <h1>Skills</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src={VBox}
+              text="Virtual Box - Virtual Machine"
+              label="Virtualization"
+              path="/VirtualBox"
+            />
+            <CardItem
+              src={ADPic}
+              text="Active Directory Domain Services"
+              label="AD"
+              path="/ActiveDirectory"
+            />
+            <CardItem
+              src={AddUser}
+              text="Active Directory Account Creation"
+              label="Add User"
+              path="/ADAccountCreation"
+            />
+          </ul>
         </div>
-    )
-}
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src={VBox}
+              text="Lab Environment w/ VirtualBox Windows Server"
+              label="Lab"
+              path="/Lab"
+            />
+            <CardItem
+              src={VBox}
+              text="Mapping Shared Folder"
+              label="Shared Folder"
+              path="/SharedFolder"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SkillSection;
--- src/components/VirtualBoxSection.js
+++ src/components/VirtualBoxSection.js
@@ -1,158 +1,184 @@
-import React from 'react'
-import VMName from '../images/NameVirtualMachine.png'
-import VMMemorySize from '../images/MemorySize.png'
-import VMHardDisk from '../images/HardDisk.png'
-import VMHardDiskType from '../images/HardDisk_Type.png'
-import VMDynamicHard from '../images/Dynamic_HardDisk.png'
-import VMHardSize from '../images/HardDisk_Size.png'
-import IsoLocation from '../images/SelectISO.png'
-import '../components/VirtualBoxSection.css'
-import ImageItem from './ImageItem'
-import Option2 from '../images/Option2.png'
-
+import React from "react";
+import VMName from "../images/NameVirtualMachine.png";
+import VMMemorySize from "../images/MemorySize.png";
+import VMHardDisk from "../images/HardDisk.png";
+import VMHardDiskType from "../images/HardDisk_Type.png";
+import VMDynamicHard from "../images/Dynamic_HardDisk.png";
+import VMHardSize from "../images/HardDisk_Size.png";
+import IsoLocation from "../images/SelectISO.png";
+import "../components/VirtualBoxSection.css";
+import ImageItem from "./ImageItem";
+import Option2 from "../images/Option2.png";
+import GreenCheck from "./GreenCheck/GreenCheck";
 
 const addingInfo = (heading, text, ImageSrc, ImageText, ImageAlt) => {
-    return (
-        <>
-            <h4>{heading}</h4>
-            <p>{text}</p>
-            <div className='image_with_caption'>
-                <ImageItem src={ImageSrc} text={ImageText} alt={ImageAlt}/>
-            </div>
-        </> 
-    )
-}
-
+  return (
+    <>
+      <h4>{heading}</h4>
+      <p>{text}</p>
+      <div className="image_with_caption">
+        <ImageItem src={ImageSrc} text={ImageText} alt={ImageAlt} />
+      </div>
+    </>
+  );
+};
 
 const VirtualBoxSection = () => {
-    const InitialSetup = addingInfo("Open Virtual Box and click New", 
-                                    "The next screen will prompt the user to name the virtual machine"+ 
-                                    "and choose the destination along with the operating system and version", 
-                                    VMName, 
-                                    "Name the Virtual Machine", 
-                                    "A prompt askign the user to name the virtual machine and choose the destiantion and version")
-    
-    const MemorySize = addingInfo("Choosing memory size", 
-                                    "It is recommended to choose at least 4 GB of memory size", 
-                                    VMMemorySize, 
-                                    "Choose desired memory size", 
-                                    "A prompt asking the user to choose the memory size for the virtual machine")
-    
-    const HDDFileType = addingInfo("Hard Disk File Type",
-                                    "Choose the desired hard disk file type",
-                                    VMHardDiskType,
-                                    "Hard Disk file type",
-                                    "A prompt asking the user to choose between VDI, VHD, and VMDK hard disk file types")
+  const InitialSetup = addingInfo(
+    "Open Virtual Box and click New",
+    "The next screen will prompt the user to name the virtual machine" +
+      "and choose the destination along with the operating system and version",
+    VMName,
+    "Name the Virtual Machine",
+    "A prompt askign the user to name the virtual machine and choose the destiantion and version"
+  );
 
-    const HDDFileSize = addingInfo("Hard Disk File Location & Size",
-                                    "Choose the desired location and size. It is recommmended to have a minimum of 16 GB for Windows. Then click 'Create'",
-                                    VMHardSize,
-                                    "VM Hard Disk file location",
-                                    "A prompt asking the user to choose between a fixed or dynamically fixed hard disk")
-    return (
-        <div className="bg_white">
-            <div className='template_container'>
-                <h1>Creating a Windows 10 Virtual Machine w/ Virtual Box</h1>
-                
-                <h4>Download the Windows media creation tool</h4>
-                
-                <p>The Windows media creation tool can be found on the microsoft website 
-                    and is necessary to download the iso file for the virtual machine
-                </p>
-                
-                <h4>Get the iso from the media creation tool</h4>
-                
-                <p>Once the media creation tool has been downloaded locate the iso file
-                    for Windows 10
-                </p>
-                
-                <h4>Download & Install Oracle's Virtual Box</h4>
-                
-                <p>Virtual Box can be found for free on the Oracle website</p>
-                
-                {InitialSetup}
-                {MemorySize}
-                
-                <p>Click radio button "<strong>Create a virtual hard disk now</strong>"</p>
+  const MemorySize = addingInfo(
+    "Choosing memory size",
+    "It is recommended to choose at least 4 GB of memory size",
+    VMMemorySize,
+    "Choose desired memory size",
+    "A prompt asking the user to choose the memory size for the virtual machine"
+  );
 
-                <div className='image_with_caption'>
-                    <ImageItem
-                        src={VMHardDisk}
-                        text="Hard Disk option"
-                        alt='A prompt asking the user to create a hard disk or use an existing one'
-                    />
-                </div>
+  const HDDFileType = addingInfo(
+    "Hard Disk File Type",
+    "Choose the desired hard disk file type",
+    VMHardDiskType,
+    "Hard Disk file type",
+    "A prompt asking the user to choose between VDI, VHD, and VMDK hard disk file types"
+  );
 
-                {HDDFileType}
-                
-                <p>Choose the way you want the storage size to be handled</p>
-                
-                <div className='image_with_caption'>
-                    <ImageItem
-                        src={VMDynamicHard}
-                        text="Dynamic or Fixed Hard Disk"
-                        alt='A prompt asking the user to choose between a fixed or dynamically fixed hard disk'
-                    />
-                </div>
-                
-                {HDDFileSize}
+  const HDDFileSize = addingInfo(
+    "Hard Disk File Location & Size",
+    "Choose the desired location and size. It is recommmended to have a minimum of 16 GB for Windows. Then click 'Create'",
+    VMHardSize,
+    "VM Hard Disk file location",
+    "A prompt asking the user to choose between a fixed or dynamically fixed hard disk"
+  );
+  return (
+    <div className="bg_white">
+      <div className="template_container">
+        <h1>Creating a Windows 10 Virtual Machine w/ Virtual Box</h1>
 
+        <h4>Download the Windows media creation tool</h4>
 
-                <h4>Now to start the virtual machine</h4>
+        <p>
+          The Windows media creation tool can be found on the microsoft website
+          and is necessary to download the iso file for the virtual machine
+        </p>
 
-                <p>Highlight the virtual machine in VirtualBox 
-                    <br />Click start on the newly created virtual machine</p>
+        <h4>Get the iso from the media creation tool</h4>
 
-                <h4>Select the iso location</h4>
-                <h4>Option 1:</h4>
+        <p>
+          Once the media creation tool has been downloaded locate the iso file
+          for Windows 10
+        </p>
 
-                <p>After hitting the start button a prompt should appear to choose the location of the iso.
-                    <br />If it doesnt, go to option 2.
-                    <br />Navigate to the ISO file
-                </p>
+        <h4>Download & Install Oracle's Virtual Box</h4>
 
-                <div className='image_with_caption'>
-                    <ImageItem
-                        src={IsoLocation}
-                        text="Locate the Windows 10 iso"
-                        alt='A prompt that asks the user select the location of the iso file'
-                    />
-                </div>
+        <p>Virtual Box can be found for free on the Oracle website</p>
 
-                <h4>Option 2:</h4>
+        {InitialSetup}
+        {MemorySize}
 
-                <p>Highlight the virtual machine in virtual box
-                <br />Click Settings {">"} Storage
-                </p>
-                
-                <div className='image_with_caption'>
-                    <ImageItem
-                        src={Option2}
-                        text="Virtual Box VM Settings"
-                        alt='Virtal Box Settings'
-                    />
-                </div>
+        <p>
+          Click radio button "<strong>Create a virtual hard disk now</strong>"
+        </p>
 
-                <p>
-                    <br />Highlight the Empty Disk
-                    <br />Then Click the disk to the right of 'Optical Drive'
-                    <br />Navigate to the ISO file then click OK
-                </p>
-
-                <h4>Now to start the virtual machine</h4>
-
-                <p>Highlight the virtual machine in VirtualBox 
-                    <br />Click start on the newly created virtual machine</p>
-                
-                <h4>Windows Setup</h4>
-                <p>Accept the license terms and go through the regular windows setup to complete
-                    and use the virtual machine for Windows 10
-                </p>
-
-            </div>
+        <div className="image_with_caption">
+          <ImageItem
+            src={VMHardDisk}
+            text="Hard Disk option"
+            alt="A prompt asking the user to create a hard disk or use an existing one"
+          />
         </div>
-    )
-}
+
+        {HDDFileType}
+
+        <p>Choose the way you want the storage size to be handled</p>
+
+        <div className="image_with_caption">
+          <ImageItem
+            src={VMDynamicHard}
+            text="Dynamic or Fixed Hard Disk"
+            alt="A prompt asking the user to choose between a fixed or dynamically fixed hard disk"
+          />
+        </div>
+
+        {HDDFileSize}
+
+        <h4>Now to start the virtual machine</h4>
+
+        <p>
+          Highlight the virtual machine in VirtualBox
+          <br />
+          Click start on the newly created virtual machine
+        </p>
+
+        <h4>Select the iso location</h4>
+        <h4>Option 1:</h4>
+
+        <p>
+          After hitting the start button a prompt should appear to choose the
+          location of the iso.
+          <br />
+          If it doesnt, go to option 2.
+          <br />
+          Navigate to the ISO file
+        </p>
+
+        <div className="image_with_caption">
+          <ImageItem
+            src={IsoLocation}
+            text="Locate the Windows 10 iso"
+            alt="A prompt that asks the user select the location of the iso file"
+          />
+        </div>
+
+        <h4>Option 2:</h4>
+
+        <p>
+          Highlight the virtual machine in virtual box
+          <br />
+          Click Settings {">"} Storage
+        </p>
+
+        <div className="image_with_caption">
+          <ImageItem
+            src={Option2}
+            text="Virtual Box VM Settings"
+            alt="Virtal Box Settings"
+          />
+        </div>
+
+        <p>
+          <br />
+          Highlight the Empty Disk
+          <br />
+          Then Click the disk to the right of 'Optical Drive'
+          <br />
+          Navigate to the ISO file then click OK
+        </p>
+
+        <h4>Now to start the virtual machine</h4>
+
+        <p>
+          Highlight the virtual machine in VirtualBox
+          <br />
+          Click start on the newly created virtual machine
+        </p>
+
+        <h4>Windows Setup</h4>
+        <p>
+          Accept the license terms and go through the regular windows setup to
+          complete and use the virtual machine for Windows 10
+        </p>
+
+        <GreenCheck />
+      </div>
+    </div>
+  );
+};
 
 export default VirtualBoxSection;
-
